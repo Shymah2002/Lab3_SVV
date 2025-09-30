485dd66f0749a5baff8ebcbe10bd5b70bc26b717
Helicopter <Sitiintern@gmail.com>
2025-09-30 15:20:12 +0800

    update additional

--- ICT3101_Calculator.UnitTests/AdditionalCalculatorTests.cs
+++ ICT3101_Calculator.UnitTests/AdditionalCalculatorTests.cs
@@ -1,12 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
+using Moq;
+using ICT3101_Calculator;
+using System.IO;
 
-namespace ICT3101_Calculator.UnitTests
+[TestFixture]
+public class AdditionalCalculatorTests
 {
-    class AdditionalCalculatorTests
+    private Calculator _calculator;
+    private Mock<IFileReader> _mockFileReader;
+
+    [SetUp]
+    public void Setup()
     {
+        _calculator = new Calculator();
+        _mockFileReader = new Mock<IFileReader>();
+
+        // Default setup for MagicNumbers.txt
+        _mockFileReader
+            .Setup(fr => fr.Read("MagicNumbers.txt"))
+            .Returns(new string[] { "42", "-3.5", "0" });
+    }
+
+    [Test]
+    public void GenMagicNum_WithMockChoice0_Returns84()
+    {
+        double result = _calculator.GenMagicNum(0, _mockFileReader.Object);
+        Assert.AreEqual(84.0, result);
+        _mockFileReader.Verify(fr => fr.Read("MagicNumbers.txt"), Times.Once);
+    }
+
+    [Test]
+    public void GenMagicNum_WithMockChoice1_Returns7()
+    {
+        double result = _calculator.GenMagicNum(1, _mockFileReader.Object);
+        Assert.AreEqual(7.0, result);
+    }
+
+    [Test]
+    public void GenMagicNum_WithMockChoiceOutOfRange_ReturnsZero()
+    {
+        double result = _calculator.GenMagicNum(999, _mockFileReader.Object);
+        Assert.AreEqual(0.0, result);
+    }
+
+    [Test]
+    public void GenMagicNum_WhenReaderThrows_FileNotFoundException()
+    {
+        _mockFileReader
+            .Setup(fr => fr.Read(It.IsAny<string>()))
+            .Throws(new FileNotFoundException());
+
+        Assert.Throws<FileNotFoundException>(() =>
+            _calculator.GenMagicNum(0, _mockFileReader.Object));
     }
 }
